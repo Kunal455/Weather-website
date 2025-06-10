1235a53af24ec6bc4729072822158840e4890618
Kunal455 <148742448+Kunal455@users.noreply.github.com>
2025-06-10 19:56:36 +0530

    Add files via upload

---
+++ wheater.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+let btns = document.querySelector(".btn");
+let searchbox = document.querySelector("#city");
+
+
+async function checkWeather(city){
+ 
+
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?&units=metric&q=${city}&appid=5653a0648b98f11431c3e8a1a89b6140`);
+    var data = await response.json();
+
+    console.log(data);
+    document.querySelector(".name").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp);
+    document.querySelector(".humid").innerHTML = Math.round(data.main.humidity);
+    document.querySelector(".press").innerHTML = Math.round(data.main.pressure);
+
+}
+
+btns.addEventListener('click',()=>{
+    checkWeather(searchbox.value);
+})
+
+
+
+
+
+
